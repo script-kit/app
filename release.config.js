f9bcdf906e5f1b05f73f33a26b63c91a888c39d2
johnlindquist <johnlindquist@gmail.com>
2021-01-23 22:22:48 -0700

    fix: generating a changelog

--- release.config.js
+++ release.config.js
@@ -2,7 +2,13 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'CHANGELOG.md',
+      },
+    ],
+
     [
       '@semantic-release/npm',
       {
@@ -13,7 +19,7 @@ module.exports = {
     [
       '@semantic-release/git',
       {
-        assets: ['src/package.json'],
+        assets: ['src/package.json', 'CHANGELOG.md'],
         message:
           // eslint-disable-next-line no-template-curly-in-string
           'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
