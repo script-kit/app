37424b2d49f0ba775720b697549367736c813e0c
johnlindquist <johnlindquist@gmail.com>
2021-01-23 17:26:29 -0700

    fix: testing semantic-release config

---
+++ release.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    [
+      '@semantic-release/npm',
+      {
+        npmPublish: false,
+        pkgRoot: 'src',
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['src/package.json'],
+        message:
+          // eslint-disable-next-line no-template-curly-in-string
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
