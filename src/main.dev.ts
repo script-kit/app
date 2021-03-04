7171e5aa76320d4e83abbceda0124e8c0e65ceb4
johnlindquist <johnlindquist@gmail.com>
2021-03-03 21:36:33 -0700

    feat(app): Startup launches prompt

--- src/main.dev.ts
+++ src/main.dev.ts
@@ -148,6 +148,8 @@ const ready = async () => {
   await createNotification();
   autoUpdater.logger = log;
   autoUpdater.checkForUpdatesAndNotify();
+
+  tryKitScript('app/left-click');
 };
 
 const options: SpawnSyncOptions = {
