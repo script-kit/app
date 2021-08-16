592d49f5c796a7fd7516133307eec3c3180527c1
John Lindquist <johnlindquist@gmail.com>
2021-08-16 11:05:06 -0600

    feat(app): force focus on mouseUp. Don't send --app flag

--- src/hooks/useFocus.ts
+++ src/hooks/useFocus.ts
@@ -1,16 +1,22 @@
 import { useAtom } from 'jotai';
 import { useEffect, useRef } from 'react';
-import { flagValueAtom, openAtom, submittedAtom } from '../jotai';
+import {
+  flagValueAtom,
+  isMouseDownAtom,
+  openAtom,
+  submittedAtom,
+} from '../jotai';
 
 export default () => {
   const ref = useRef<HTMLElement>();
   const [flagValue] = useAtom(flagValueAtom);
   const [submitted] = useAtom(submittedAtom);
   const [open] = useAtom(openAtom);
+  const [mouseDown] = useAtom(isMouseDownAtom);
 
   useEffect(() => {
     if (ref?.current) ref?.current.focus();
-  }, [flagValue, submitted, open]);
+  }, [flagValue, submitted, open, mouseDown]);
 
   return ref;
 };
