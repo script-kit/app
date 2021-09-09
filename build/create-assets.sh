739eb4532c4c57698f18e9d5c788a7d90de9e07d
John Lindquist <johnlindquist@gmail.com>
2021-09-09 13:13:22 -0600

    fix(build): release_channel to assets

--- build/create-assets.sh
+++ build/create-assets.sh
@@ -6,6 +6,8 @@ release_channel="${release_channel:="$(git rev-parse --abbrev-ref HEAD)"}"
 echo "Release channel: $release_channel"
 kit_dir="$WD/node_modules/@johnlindquist/kit"
 
+echo "$release_channel" >> "$WD/assets/release_channel.txt"
+
 # Into to $kit_dir
 cd $kit_dir
 zip -r "$WD/assets/kit.zip" ./ -x "./node_modules/*" -x "./node/*"
