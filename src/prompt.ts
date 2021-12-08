09d36b58882a238da9aee76b6bd54fe41d280ccc
John Lindquist <johnlindquist@gmail.com>
2021-12-07 22:55:09 -0700

    feat(prompt): polish

--- src/prompt.ts
+++ src/prompt.ts
@@ -68,7 +68,7 @@ export const createPromptWindow = async () => {
       nodeIntegration: true,
       contextIsolation: false,
       devTools: process.env.NODE_ENV === 'development' || devTools,
-      backgroundThrottling: true,
+      backgroundThrottling: false,
     },
     alwaysOnTop: true,
     closable: false,
