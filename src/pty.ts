0c7bbb65be7d65267eace5d0b2600170c7ff674d
John Lindquist <johnlindquist@gmail.com>
2022-12-24 09:42:27 -0700

    fix(pty): remove env

--- src/pty.ts
+++ src/pty.ts
@@ -48,7 +48,6 @@ export const startPty = async (config: any = {}) => {
       cwd: untildify(config?.cwd || os.homedir()),
       encoding: USE_BINARY ? null : 'utf8',
       env: {
-        ...process.env,
         PATH: KIT_FIRST_PATH,
       },
     }
