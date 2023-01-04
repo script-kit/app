e521a3d47026cbf2d50e6af8fec404f0e7331ff5
suren-atoyan <contact@surenatoyan.com>
2023-01-04 11:39:00 +0400

    use MonacoSetup component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,13 @@
 import IpcBridge from './ipc-bridge';
 import Container from './container';
-
-import MonacoEditor, { loader } from '@monaco-editor/react';
-import * as monaco from 'monaco-editor';
-import { useEffect, useState } from 'react';
-loader.config({ monaco });
+import MonacoSetup from './monaco/Setup';
 
 export default function App() {
-  const [start, setStart] = useState(false);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setStart(true);
-    }, 100);
-  }, []);
-
   return (
     <div>
-      {start ? (
-        <>
-          <IpcBridge />
-          <Container />
-        </>
-      ) : (
-        <div style={{ display: 'none' }}>
-          {/* Hack: https://github.com/johnlindquist/kitapp/issues/200 */}
-          <MonacoEditor
-            language="typescript"
-            options={{
-              accessibilitySupport: 'off',
-              ariaContainerElement: document.getElementById(
-                'a11y'
-              ) as HTMLElement,
-              renderLineHighlight: 'none',
-            }}
-          />
-        </div>
-      )}
+      <IpcBridge />
+      <Container />
+      <MonacoSetup />
     </div>
   );
 }
-
-// .monaco-editor .view-overlays .current-line
