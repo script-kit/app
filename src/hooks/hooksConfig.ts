7513262ab599c5cf0a29d8dd2fe23024dafe2ca0
suren-atoyan <contact@surenatoyan.com>
2023-01-05 16:52:33 +0400

    filter keys that should be ignored while typing in monaco

--- src/hooks/hooksConfig.ts
+++ src/hooks/hooksConfig.ts
@@ -1,5 +1,20 @@
 import { Options } from 'react-hotkeys-hook';
 
+const KEYS_TO_IGNORE_WHILE_IN_MONACO = ['Enter'];
+
 export const hotkeysOptions: Options = {
   enableOnTags: ['INPUT', 'TEXTAREA', 'SELECT'],
+  filterPreventDefault: false,
+  filter: (event: KeyboardEvent) => {
+    const target = event?.target as Element;
+    if (
+      // TODO: find a better way to identify that it's being triggered from monaco
+      target?.classList.value.includes('monaco') &&
+      KEYS_TO_IGNORE_WHILE_IN_MONACO.includes(event.code)
+    ) {
+      return false;
+    }
+
+    return true;
+  },
 };
