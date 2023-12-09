cbaf8aecf456a4a56fdd961db5bc7664c3871c73
John Lindquist <johnlindquist@gmail.com>
2023-12-09 16:10:28 -0700

    fix(build): afterPack

--- .erb/scripts/AfterPack.js
+++ .erb/scripts/AfterPack.js
@@ -15,11 +15,13 @@ exports.default = async function afterPack(context) {
     archName,
   });
   if (electronPlatformName === 'linux') {
+    console.log(`Rebuilding node-pty for ${archName}...`);
     const rebuildCmd = `./node_modules/.bin/electron-rebuild --arch=${archName} --module-dir ${path.join(
-      appOutDir,
-      'resources',
-      'src'
+      'src',
+      'node_modules',
+      'node-pty'
     )}`;
     execSync(rebuildCmd, { stdio: 'inherit' });
+    console.log(`Rebuilt node-pty for ${archName} 🛠`);
   }
 };
