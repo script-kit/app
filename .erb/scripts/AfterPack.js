fe50539807edc7e444fcaf5308d73fdee9b2746d
John Lindquist <johnlindquist@gmail.com>
2023-12-09 15:41:06 -0700

    fix(build): afterPack

--- .erb/scripts/AfterPack.js
+++ .erb/scripts/AfterPack.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const { execSync } = require('child_process');
+const { Arch } = require('electron-builder');
 
 exports.default = async function afterPack(context) {
   const { appOutDir, electronPlatformName, arch } = context;
 
+  const archName = Arch.arm64 ? 'arm64' : 'x64';
   // Only rebuild for Linux ARM64
   if (electronPlatformName === 'linux') {
-    const rebuildCmd = `./node_modules/.bin/electron-rebuild --arch=${arch} --module-dir ${path.join(
+    const rebuildCmd = `./node_modules/.bin/electron-rebuild --arch=${archName} --module-dir ${path.join(
       appOutDir,
       'resources',
       'app'
